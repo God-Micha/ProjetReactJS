2a5bb9b3e826d7b97e2170ae9896d8e145798654
AlexisMalattia <alexis.malattia06@gmail.com>
2024-02-17 15:09:08 +0100

    Initialisation du canvas

---
+++ front/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Canvas from "./components/canvas/Canvas";
+
+function App() {
+  return (
+      <div className="App">
+        <h1>Mon Place</h1>
+        <Canvas />
+      </div>
+  );
+}
+
+export default App;
