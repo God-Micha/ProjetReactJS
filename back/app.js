c805f0c59add028bd73b7e97c6b2cd6be2a68374
Elodiebantos <56624671+Elodiebantos@users.noreply.github.com>
2024-04-01 21:48:58 +0200

    [Elodie] BD + homepage fix

--- back/app.js
+++ back/app.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
+const mongo = require('./mongo');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+	next();
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`server listening at http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Goddees Elo is saying you Hello!');
+});
+
+app.get('/users', (req, res) => {
+	mongo.getUsers()
+		.then(data => {
+			//console.log(JSON.stringify(data))
+			res.send(JSON.stringify(data));
+		});
+});
+
+app.get('/pixelboards', (req, res) => {
+	mongo.getPixelboards()
+		.then(data => {
+			res.send(JSON.stringify(data));
+		});
 });
