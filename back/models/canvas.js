6e9f344167369fb5b6a0b85c70704340c79130e7
AlexisMalattia <alexis.malattia06@gmail.com>
2024-04-07 17:09:09 +0200

    Vérification des champs et enregistrement du token

--- back/models/canvas.js
+++ back/models/canvas.js
@@ -5,10 +5,43 @@ const canvasSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Chunk',
     }],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
     updatedAt: {
-            type: Date,
-            default: Date.now,
-        },
+        type: Date,
+        default: Date.now,
+    },
+    endDate: {
+        type: Date,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['active', 'inactive'],
+        default: 'active',
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    editDelay: {
+        type: Number,
+        required: true,
+    },
 });
+canvasSchema.pre('save', function(next) {
+    if (this.endDate < Date.now()) {
+        this.status = 'inactive';
+    }
+    next();
+});
+
 const Canvas = mongoose.model('Canvas', canvasSchema);
 module.exports = Canvas;
